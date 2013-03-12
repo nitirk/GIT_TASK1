d6ca184ae13d92b3df383a6b61b90f6e657b52d5
brawbraw <kunbigball@gmail.com>
2013-03-12 19:49:24 +0700

    Update incomplete_code.py

--- incomplete_code.py
+++ incomplete_code.py
@@ -26,6 +26,7 @@ class Train( Transportation ):
    def __init__( self, start, end, distance , station):
       Transportation.__init__( self, start, end, distance)
       self.a = station
+      
    def find_cost( self):
       return self.a * 5
       
